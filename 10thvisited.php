a836a9be2fab4fb7aa9add8a50855ffd946846d9
Prathamesh Thakur <pratham.thakur2902@gmail.com>
2022-04-20 22:18:56 +0530

    Latest updates

--- 10thvisited.php
+++ 10thvisited.php
@@ -4,11 +4,9 @@ session_start();
 <html>
     <body>
     <?php 
-    
-
-$txtname1 = $_SESSION['usrname'];
-$visited = "10th Standard";
-include_once("connect.php");
+    $txtname1 = $_SESSION['usrname'];
+    $visited = "10th Standard";
+    include_once("connect.php");
 
 $sql = "INSERT INTO user_visited VALUES('$txtname1', '$visited')";
 mysqli_query($conn, $sql);
--- Mastersvisited.php
+++ Mastersvisited.php
@@ -4,16 +4,13 @@ session_start();
 <html>
     <body>
     <?php 
-    
+    $txtname2 = $_SESSION['usrname'];
+    $visited = "Masters";
+    include_once("connect.php");
 
-$txtname1 = $_SESSION['usrname'];
-$visited = "Master's";
-include_once("connect.php");
-
-$sql = "INSERT INTO user_visited VALUES('$txtname1', '$visited')";
-mysqli_query($conn, $sql);
-
-header("Location:Mastershomepage.html");
+    $sql = "INSERT INTO user_visited VALUES('$txtname2', '$visited')";
+    mysqli_query($conn, $sql);
+    header("Location:Mastershomepage.html");
 ?>
     </body>
 </html>
